9a89217363fd191516170561e2750cb4747baf09
Vu Hoang <hoangmeo1905@gmail.com>
2024-01-30 15:18:36 +0700

    initialize folders

---
+++ JS/halloffame.js
@@ -0,0 +1,71 @@
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+
+function convertTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.round(time % 60);
+    return minutes + "m" + seconds + "s";
+}
+
+for (var i = 1; i <= 10; ++i) {
+    let score = localStorage.getItem("hofScore" + i);
+    let timeTaken = localStorage.getItem("hofTime" + i);
+    timeTaken = convertTime(timeTaken);
+    let state = localStorage.getItem("hofState" + i);
+
+    let hofTable = document.getElementById("hof");
+    let row = document.createElement("tr");
+
+    let position = document.createElement("td");
+    position.textContent = i;
+
+    let playerScore = document.createElement("td");
+    playerScore.textContent = score;
+
+    let playerTime = document.createElement("td");
+    playerTime.textContent = timeTaken;
+
+    let playerState = document.createElement("td");
+    playerState.textContent = state;
+
+    switch (i) {
+        case 1:
+            position.className = "first-place";
+            position.textContent = i + 'st';
+            playerScore.className = "first-place";
+            playerTime.className = "first-place";
+            playerState.className = "first-place";
+            break;
+        case 2:
+            position.className = "second-place";
+            position.textContent = i + 'nd';
+            playerScore.className = "second-place";
+            playerTime.className = "second-place";
+            playerState.className = "second-place";
+            break;
+        case 3:
+            position.className = "third-place";
+            position.textContent = i + 'rd';
+            playerScore.className = "third-place";
+            playerTime.className = "third-place";
+            playerState.className = "third-place";
+            break;
+        default:
+            position.className = "other";
+            position.textContent = i + 'th';
+            playerScore.className = "other";
+            playerTime.className = "other";
+            playerState.className = "other";
+    }
+
+    row.appendChild(position);
+    row.appendChild(playerScore);
+    row.appendChild(playerTime);
+    row.appendChild(playerState);
+    hofTable.appendChild(row);
+}
+
+function pad(number) {
+    return number < 10 ? "0" + number : number;
+}
